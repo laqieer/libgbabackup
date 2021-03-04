4d6941cb694abb2d948b142efd1ba884f681e004
laqieer <laqieer@126.com>
2021-03-04 13:26:53 +0800

    4kb/64kb Eeprom Lib
        * EEPROM_V124 decompiled from Legend of Zelda, The - The Minish Cap

---
+++ include/eeprom.h
@@ -0,0 +1,24 @@
+#ifndef GUARD_EEPROM_H
+#define GUARD_EEPROM_H
+
+#include "types.h"
+#include "io_reg.h"
+
+// error codes
+#define EEPROM_OUT_OF_RANGE 0x80ff
+#define EEPROM_COMPARE_FAILED 0x8000
+#define EEPROM_UNSUPPORTED_TYPE 0x8080
+
+/**
+ * selects EEPROM type
+ * selects 512byte on invalid argument
+ *
+ * @param unk_1 4 for 512 byte, 0x40 for 8k
+ * @return 1 on invalid argument, 0 otherwise
+ */
+u16 EEPROMConfigure(u16 unk_1);
+u16 EEPROMRead(u16 address, u16* data);
+u16 EEPROMCompare(u16 address, const u16* data);
+u16 EEPROMWrite0_8k_Check(u16 address, const u16* data);
+
+#endif // GUARD_EEPROM_H
