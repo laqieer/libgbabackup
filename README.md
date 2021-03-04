1ede55ca9a1d4f3d1076c556bac2ba225af87260
laqieer <laqieer@126.com>
2021-03-04 11:36:57 +0800

    256kb SRAM Lib
    * SRAM_V113 decompiled from Kirby & The Amazing Mirror

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Completed
 * 256kbit SRAM High Speed Access Library ver. 1.0.3 (SRAM_F_V103) from [fireemblem8u](https://github.com/FireEmblemUniverse/fireemblem8u)
 * 1Mbit FLASH Library ver. 1.0.3 (FLASH1M_V103) from [pokeruby](https://github.com/pret/pokeruby)
-* 256kbit SRAM Library ver. 1.1.3 (SRAM_V113)
+* 256kbit SRAM Library ver. 1.1.3 (SRAM_V113) from [katam](https://github.com/jiangzhengwenjz/katam)
 * 512kbit FLASH Library ver. 1.3.1 (FLASH512_V131)
 
 ## ToDo
--- src/agb_sram.c
+++ src/agb_sram.c
@@ -1,27 +1,31 @@
 #include "agb_sram.h"
 
-static const char AgbLibSramVersion[] = "SRAM_V113";
+const char gAgbSramLibVer[] = "NINTENDOSRAM_V113";
 
-void ReadSram_Core(const u8 *src, u8 *dest, u32 size)
+static void ReadSram_Core(const u8 *src, u8 *dest, u32 size)
 {
     while (--size != -1)
         *dest++ = *src++;
 }
 
-void ReadSram(const u8 *src,u8 *dest,u32 size)
+void ReadSram(const u8 *src, u8 *dest, u32 size)
 {
-    u16 i,readSram_Core_Work[0x40],*pSrc,*pDest;
-    void (*readSram_p)(const u8 *,u8 *,u32);
+    const u16 *s;
+    u16 *d;
+    u16 readSramFast_Work[64];
+    u16 size_;
 
     REG_WAITCNT = (REG_WAITCNT & ~3) | 3;
-    pSrc=(u16 *)ReadSram_Core;
-    pSrc=(u16 *)((u32)pSrc & 0xFFFFFFFE);
-    pDest=readSram_Core_Work;
-    for(i=((u32)ReadSram-(u32)ReadSram_Core)>>1;i>0;i--)
-        *pDest++=*pSrc++;
-    readSram_p=(void (*)(const u8 *,u8 *,u32))((u8 *)readSram_Core_Work+1);
-
-    readSram_p(src,dest,size);
+    s = (void *)((uintptr_t)ReadSram_Core);
+    s = (void *)((uintptr_t)s & ~1);
+    d = readSramFast_Work;
+    size_ = ((uintptr_t)ReadSram - (uintptr_t)ReadSram_Core) / 2;
+    while (size_ != 0)
+    {
+        *d++ = *s++;
+        --size_;
+    }
+    ((void (*)(const u8 *, u8 *, u32))readSramFast_Work + 1)(src, dest, size);
 }
 
 void WriteSram(const u8 *src, u8 *dest, u32 size)
@@ -31,45 +35,47 @@ void WriteSram(const u8 *src, u8 *dest, u32 size)
         *dest++ = *src++;
 }
 
-u32 VerifySram_Core(const u8 *src, u8 *dest, u32 size)
+static u32 VerifySram_Core(const u8 *src, u8 *dest, u32 size)
 {
     while (--size != -1)
-    {
         if (*dest++ != *src++)
             return (u32)(dest - 1);
-    }
     return 0;
 }
 
-u32 VerifySram(const u8 *src,u8 *dest,u32 size)
+u32 VerifySram(const u8 *src, u8 *dest, u32 size)
 {
-    u16 i,verifySram_Core_Work[0x60],*pSrc,*pDest;
-    u32 (*verifySram_p)(const u8 *,u8 *,u32);
+    const u16 *s;
+    u16 *d;
+    u16 verifySramFast_Work[96];
+    u16 size_;
 
     REG_WAITCNT = (REG_WAITCNT & ~3) | 3;
-    pSrc=(u16 *)VerifySram_Core;
-    pSrc=(u16 *)((u32)pSrc & 0xFFFFFFFE);
-    pDest=verifySram_Core_Work;
-    for(i=((u32)VerifySram-(u32)VerifySram_Core)>>1;i>0;i--)
-        *pDest++=*pSrc++;
-    verifySram_p=(u32 (*)(const u8 *,u8 *,u32))((u8 *)verifySram_Core_Work+1);
-
-    return verifySram_p(src,dest,size);
+    s = (void *)((uintptr_t)VerifySram_Core);
+    s = (void *)((uintptr_t)s & ~1);
+    d = verifySramFast_Work;
+    size_ = ((uintptr_t)VerifySram - (uintptr_t)VerifySram_Core) / 2;
+    while (size_ != 0)
+    {
+        *d++ = *s++;
+        --size_;
+    }
+    return ((u32 (*)(const u8 *, u8 *, u32))verifySramFast_Work + 1)(src, dest, size);
 }
 
-u32 WriteAndVerifySram(const u8 *src, u8 *dest, u32 size)
+u32 WriteSramEx(const u8 *src, u8 *dest, u32 size)
 {
     u8 i;
     u32 errorAddr;
 
     // try writing and verifying the data 3 times
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < SRAM_RETRY_MAX; ++i)
     {
         WriteSram(src, dest, size);
         errorAddr = VerifySram(src, dest, size);
         if (errorAddr == 0)
             break;
     }
-
     return errorAddr;
 }
+
